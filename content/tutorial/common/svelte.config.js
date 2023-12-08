b4571234c9cac3d48a8131fe66d5f6e8777eae94
mfw78 <mfw78@protonmail.com>
2023-12-08 14:03:12 +0000

    chore: tutorial environment

--- content/tutorial/common/svelte.config.js
+++ content/tutorial/common/svelte.config.js
@@ -1,11 +1,15 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// For the tutorial, we need to disable CSRF protection.
 		// Don't do this in your own apps unless you know what you're doing!
 		// See https://kit.svelte.dev/docs/configuration#csrf for more info.
-		csrf: false
+		csrf: false,
 	},
+
+	preprocess: [vitePreprocess()],
 
 	vitePlugin: {
 		// This enables compile-time warnings to be

a57b9741eabb81ed75a6816cd9f6c9d33c71de82
shoom3301 <shoom3301@gmail.com>
2023-12-11 13:43:48 +0600

    chore: fix build with cow-sdk

--- content/tutorial/01-svelte/01-introduction/01-welcome-to-svelte/app-a/src/lib/App.svelte
+++ content/tutorial/01-svelte/01-introduction/01-welcome-to-svelte/app-a/src/lib/App.svelte
@@ -1,1 +1,7 @@
+<script lang="ts">
+  import { OrderBookApi } from '@cowprotocol/cow-sdk';
+
+  console.log('OrderBookApi', OrderBookApi)
+</script>
+
 <h1>Welcome!</h1>
--- content/tutorial/common/vite.config.ts
+++ content/tutorial/common/vite.config.ts
@@ -2,8 +2,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-
+	plugins: [
+		sveltekit()
+	],
+	resolve: {
+		alias: {
+			stream: 'stream-browserify'
+		}
+	},
+	define: {
+		global: 'global',
+	},
 	// Normally this would be unnecessary, but we
 	// need it for learn.svelte.dev
 	server: {

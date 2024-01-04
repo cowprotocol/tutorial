a58e5877313c5f646e92dd3136193d814b67840e
fairlighteth <31534717+fairlighteth@users.noreply.github.com>
2024-01-04 15:56:45 +0000

    feat: styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	import { Icon, Shell } from '@sveltejs/site-kit/components';
 	import { Nav, Separator } from '@sveltejs/site-kit/nav';
 	import { Search, SearchBox } from '@sveltejs/site-kit/search';
-	// import '@sveltejs/site-kit/styles/index.css';
+	import '@sveltejs/site-kit/styles/index.css';
 	import '../app.css';
 
 	export let data;

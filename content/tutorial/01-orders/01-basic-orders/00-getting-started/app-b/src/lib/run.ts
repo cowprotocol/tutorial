b0c186d95b4f4b84f81e3fef80494fc88ee87b16
mfw78 <mfw78@protonmail.com>
2024-01-05 09:44:46 +0000

    chore: file move lost in push

--- content/tutorial/01-orders/01-basic-orders/00-getting-started/app-b/src/lib/run.ts
+++
@@ -1,8 +0,0 @@
-import type { Web3Provider } from '@ethersproject/providers'
-
-export async function run(provider: Web3Provider): Promise<unknown> {
-  console.log('Hello world!');
-  return {
-    message: "Hello world!"
-  };
-}
--- content/tutorial/01-orders/01-basic-orders/01-approve/app-a/src/lib/run.ts
+++
@@ -1,5 +0,0 @@
-import type { Web3Provider } from '@ethersproject/providers'
-
-export async function run(provider: Web3Provider): Promise<unknown> {
-  // TODO: Implement
-}
